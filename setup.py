ab1ceba4b284184a3c01c07c939f9f99ca8bfd2a
Andrei <andrei@edumo.org>
2019-12-18 11:34:16 +0200

    Bumped up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 # tutorial: 
 # https://python-packaging.readthedocs.io/en/latest/index.html
 setup(name='mobilpy',
-        version='0.3',
+        version='0.4',
         description='Library that helps create the XML necessary for online payments with MobilPay',
         long_description=readme(),
         keywords='online payments mobilpay netopia',
@@ -33,5 +33,4 @@ setup(name='mobilpy',
             'pyOpenSSL',
             'pycryptodome'
         ],
-        # include_package_data=True,
         zip_safe=False)
